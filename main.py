1b59b4495b7506c36e7035edf4432aa3401f6d9b
Fedt2407 <federico.tognetti@gmail.com>
2024-07-11 15:43:51 +0200

    Final code refactoring

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template
 from flask import request
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
-
-@app.route('/user_stopped_typing', methods=['POST'])
-def user_stopped_typing():
-    data = request.get_json()
-    print("User stopped typing. Text content:", data['text'])
-    return jsonify({"message": "Received text content"})
 
 
 if __name__ == '__main__':
