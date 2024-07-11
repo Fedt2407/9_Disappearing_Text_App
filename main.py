960036b21c4987e549bedc086409e831fb3bd90c
Fedt2407 <federico.tognetti@gmail.com>
2024-07-11 15:33:54 +0200

    Added countdown function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+from flask import request
 
 app = Flask(__name__)
 
@@ -6,6 +7,11 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/user_stopped_typing', methods=['POST'])
+def user_stopped_typing():
+    data = request.get_json()
+    print("User stopped typing. Text content:", data['text'])
+    return jsonify({"message": "Received text content"})
 
 
 if __name__ == '__main__':
